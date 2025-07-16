9e14f0b1db46f0a9d84f6eaee93ca0100d941e1b
Darsh-Bothra <darshbothra007@gmail.com>
2025-07-16 19:54:12 +0530

    Redirection to sign-in home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,14 @@
 import Navbar from '@/components/NavBar'
-// import { isAuthenticated } from '@/lib/action/auth.action'
+import { isAuthenticated } from '@/lib/action/auth.action'
 import { AuthProvider } from '@/context/AuthContext'
 import LoginModal from '@/components/Modal'
+import { redirect } from 'next/navigation'
 
 const HomeLayout = async ({ children }: { children: React.ReactNode }) => {
-    // const isUserAuthenticated = await isAuthenticated();
+    const isUserAuthenticated = await isAuthenticated();
+    if(!isUserAuthenticated) {
+        redirect('/sign-in');
+    }
     return (
         <div className='root-layout'>
             <AuthProvider>
